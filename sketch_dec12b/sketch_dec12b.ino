cd8e8f6ba470fdf7b9cbfd069df97ae02da9f76a
kumru123 <147985919+kumru123@users.noreply.github.com>
2023-12-12 14:15:25 +0300

    Add files via upload

---
+++ sketch_dec12b/sketch_dec12b.ino
@@ -0,0 +1,14 @@
+void setup() {
+Serial.begin(9600);
+pinMode(6,OUTPUT);//Trig
+pinMode(7,INPUT); //Echo
+}
+void loop() {
+digitalWrite(6,1); delay(1);
+digitalWrite(6,0);
+int sure=pulseIn(7,1);
+int mesafe=(sure/2)/28.97;
+Serial.print("Olculen Mesafe=");
+Serial.println(mesafe);
+delay(1);
+}
